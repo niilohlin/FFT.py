2ffc0d9d0c0ddfbe1ba09c2a05be0f8f8e2626f8
Niil Öhlin <niil.ohlin@atea.se>
2018-02-15 04:47:37 +0100

    Add discrete and fast fourier transform

--- fourier.py
+++ fourier.py
@@ -1,4 +1,3 @@
-
 import scipy.integrate as integrate
 import scipy
 import numpy as np
@@ -12,16 +11,11 @@ def complex_quadrature(func, a, b, **kwargs):
     imag_integral = integrate.quad(imagFunc, a, b, **kwargs)
     return (real_integral[0] + 1j * imag_integral[0], real_integral[1:], imag_integral[1:])
 
-def plot(f):
-    xValues = list(map(lambda x: x / 10, range(-100, 100)))
-    yValues = list(map(f, xValues))
-    plt.plot(xValues, yValues)
+xValues = list(map(lambda x: x / 10, range(0, 1024)))
+#xValues = list(range(-10, 10))
 
-def plot_complex(f):
-    tValues = map(lambda x: x / 10, range(-100, 100))
-    complexValues = map(f, tValues)
-    xValues = list(map(scipy.real, complexValues))
-    yValues = list(map(scipy.imag, complexValues))
+def plot(f):
+    yValues = list(map(f, xValues))
     plt.plot(xValues, yValues)
 
 def fourier(f, xi, minimum=-np.inf, maximum=np.inf):
@@ -29,9 +23,47 @@ def fourier(f, xi, minimum=-np.inf, maximum=np.inf):
 
     return np.absolute(complex_quadrature(func, minimum, maximum)[0])
 
+def list_to_function(lst):
+    return lambda x: lst[int(x)] if 0 <= int(x) < len(lst) else 0
+
+def function_to_list(f):
+    return list(map(f, xValues))
+
+def discrete_fourier(array):
+    array = np.array(array)
+    length = array.shape[0]
+    n = np.arange(length)
+    k = n.reshape((length, 1))
+    M = np.exp(-2j * np.pi * k * n / length)
+
+    return np.dot(M, array)
+
+def fast_fourier_transform(array):
+    x = np.asarray(array, dtype=float)
+    N = x.shape[0]
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return discrete_fourier(x)
+    else:
+        X_even = fast_fourier_transform(x[::2])
+        X_odd = fast_fourier_transform(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N // 2] * X_odd,
+                               X_even + factor[N // 2:] * X_odd])
+
+def peak(x, size=1):
+    if -size <= x <= 0:
+        return x + size
+    if 0 <= x <= size:
+        return size - x
+    return 0
+
 if __name__ == "__main__":
-    func = lambda x: 1 if -1 < x < 1 else 0
-    hat = lambda xi: fourier(func, xi)
+    func = lambda x: peak(x, size=10)
+    #hat = lambda xi: fourier(func, xi, min(xValues), max(xValues))
+    hat = list_to_function(fast_fourier_transform(function_to_list(peak)))
     plot(func)
     plot(hat)
     plt.show()
